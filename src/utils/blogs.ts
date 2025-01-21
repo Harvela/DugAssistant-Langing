5cb19d2aec478ab4188ea0810b09c1ba38deecdc
ciraganenicole <ciraganenicole@gmail.com>
2025-01-20 19:06:17 -0800

    Update

--- src/utils/blogs.ts
+++ src/utils/blogs.ts
@@ -1,28 +1,39 @@
 export const blogs = [
   {
     id: '1',
+    image: '/assets/blog/math.jpg',
+    author: 'Ciragane Nicole',
+    date: '19 Jan 2025',
+    title: 'Un Équilibre entre IA et Livres Scolaires',
+    tags: ['Education', 'Intelligence artificielle', 'Technologie', 'Livres'],
+    link: 'https://medium.com/@harvelyc/un-%C3%A9quilibre-parfait-entre-ia-et-livres-scolaires-06bd42b01c43',
+    description:
+      'DugAssistant combine les livres scolaires et l’IA pour générer des fiches pédagogiques adaptées aux réalités des classes congolaises. ...',
+  },
+  {
+    id: '2',
     image: '/assets/blog/article2.jpeg',
     author: 'Ciragane Nicole',
     date: '1 Nov 2024',
     title: "Réussir la Transition vers l'APC/APS en RDC",
-    tags: ['Education', 'Intelligence artificielle'],
+    tags: ['Education', 'Intelligence artificielle', 'Compétences'],
     link: 'https://medium.com/@harvelyc/r%C3%A9ussir-la-transition-vers-lapc-aps-en-rdc-avec-dugassistant-b6cfec747ebd',
     description:
-      'La mise en œuvre du système éducatif basé sur les Approches par Compétences et par Situation ...',
+      'La mise en œuvre du système éducatif basé sur les Approches par Compétences (APC) et par Situation (APS) ...',
   },
   {
-    id: '2',
+    id: '3',
     image: '/assets/blog/article1.jpeg',
     author: 'Ciragane Nicole',
     date: '5 Nov 2024',
     title: 'L’impact de DugAssistant sur les enseignantes',
-    tags: ['Femmes', 'Education'],
+    tags: ['Femmes', 'Education', 'Numérisation'],
     link: 'https://medium.com/@harvelyc/limpact-de-dugassistant-sur-les-enseignantes-un-soutien-essentiel-pour-concilier-travail-et-vie-97e1b8d7bfd9',
     description:
       'Les enseignantes jouent un rôle clé dans le système éducatif en République Démocratique du Congo.',
   },
   {
-    id: '3',
+    id: '4',
     image: '/assets/blog/article3.jpeg',
     author: 'Ciragane Nicole',
     date: '12 Nov 2024',
@@ -33,7 +44,7 @@ export const blogs = [
       'Les élèves, souvent confrontés à des ressources limitées, ont besoin d’une approche éducative plus interactive',
   },
   {
-    id: '4',
+    id: '5',
     image: '/assets/blog/article4.png',
     author: 'Ciragane Nicole',
     date: '17 Nov 2024',
@@ -44,7 +55,7 @@ export const blogs = [
       'DugAssistant Prépare les Élèves Congolais aux Compétences du 21e Siècle',
   },
   {
-    id: '5',
+    id: '6',
     image: '/assets/blog/article5.png',
     author: 'Ciragane Nicole',
     date: '22 Nov 2024',
